b1852cc3ac89005738837992633ce237d24dd849
cossack-don <ser90593696.zhuravlev@yandex.ru>
2021-05-10 22:17:35 +0300

    add discription for vue-router xyk before-enter

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,3 +4,44 @@
   </div>
 </template>
 
+// для регистрации , вошел под учеткой или нет
+// будем ли пускать на данный роут или нет, роутинговый хук beforeEnter
+
+auth: false
+
+create() {
+  this.auth = localStorage.getItem('auth') ! == null
+}
+
+methods: {
+changeUserState() {
+  if(this.auth) {
+    localStorage.removeItem('auth')
+    this.router.push({name:'main'})
+  } else {
+    localStorage.setItem('auth', true)
+    this.auth = true
+  }
+}
+}
+
+// далее
+в router index js 
+
+// например путь
+    {
+      path: ':id',
+      name: 'AboutItem',
+      component: AboutItem,
+      beforeEnter:(to, from, next) => {
+
+        // если все окей,пользователь зашел под нужной учеткой, пускаем смотреть роут
+        if(localStorage.getItem('auth')) {
+          next()
+        } else {
+          // редиректим на страницу about
+          next({name:'about'})
+        }
+        
+      }
+    },
